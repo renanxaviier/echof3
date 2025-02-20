b6dcf65f07fa42b90e03183ba0580b52d5a45a8c
Renan Xaviier <116590892+renanxaviier@users.noreply.github.com>
2025-02-20 13:01:48 -0300

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,82 @@
+/*!
+* Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    //  Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+<script>
+function sendToWhatsApp() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let message = document.getElementById("message").value;
+
+    if (name === "" || email === "" || phone === "" || message === "") {
+        alert("Por favor, preencha todos os campos.");
+        return;
+    }
+
+    let whatsappNumber = "5585997573629"; // Substitua pelo número do seu WhatsApp com DDD
+    
+    let text = `Olá, meu nome é *${name}*.
+Meu e-mail é: ${email}.
+Meu WhatsApp: ${phone}.
+
+Mensagem: 
+${message}`;
+
+    let encodedText = encodeURIComponent(text);
+    let whatsappURL = `https://wa.me/5585997573629?text=${encodedText}`;
+
+    window.open(whatsappURL, "_blank"); // Abre o WhatsApp em uma nova aba/janela
+}
+</script>
